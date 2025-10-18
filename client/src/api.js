3dc036e58711eaf2adc35b279157f189e30c1477
vijaychavda5411 <vijaychavda5411@gmail.com>
2025-10-18 22:37:25 +0530

    slove  downlodd pdf

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,4 @@
-import axios from "axios";
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
-axios.get(`${API_BASE_URL}/api/resume`)
-  .then(res => console.log(res.data))
-  .catch(err => console.error(err));
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default API_BASE_URL;
